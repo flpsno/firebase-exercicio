7759dce17c377150836e439c4ab16df48a190b02
flpsno <flpsno@hotmail.com>
2017-12-23 13:10:16 -0200

    primeiro commit

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAfyELhfxRw7FWrdqjTG8jDBNgc1c2RwNk",
+    authDomain: "dipapel-163218.firebaseapp.com",
+    databaseURL: "https://dipapel-163218.firebaseio.com",
+    projectId: "dipapel-163218",
+    storageBucket: "dipapel-163218.appspot.com",
+    messagingSenderId: "685288768922"
+};
+firebase.initializeApp(config);
